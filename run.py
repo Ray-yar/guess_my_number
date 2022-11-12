bb84cffd488bf93dbe2feaf2506ca64de3371d0a
Rohullah Ahmadyar <Rohullah.yar786@gmail.com>
2022-11-12 02:45:45 +0000

    Initial Git

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("Guess My Number is an interesting game to play.")
+print("You just need to guess the number we assigned, any time that you fail then your number will be decreased.")
+print("Your number will start from 20 and it will decrease if you answer wrong number")
+print("")
+print("Lets Start!!!")
+print("")
+
+def get_user_answer():
+    """
+        In This Function we need to get data from user.
+    """
+    user_value = input("Enter the correct number:\n")
+    return user_value
+
+
+get_user_answer()
